427fdc8f87d9b9123c799ea608926663ef58da0e
stefan <https://github.com/Stefan03404/Stefan03404.git>
2022-08-23 19:01:26 +0300

    Оффисный учёт данных

---
+++ list office.py
@@ -0,0 +1,17 @@
+count_of_workers = int(input("Кол-во сотрудников в офисе: "))
+workers_id = []
+for _ in range(count_of_workers):
+    worker_id = int(input("ID сотрудника: "))
+    workers_id.append(worker_id)
+
+search_id = int(input("Какого сотрудника ищем? "))
+
+search = False
+for id in workers_id:
+    if id == search_id:
+        search = True
+
+if search:
+    print("Сотрудник работает!")
+else:
+    print("Сотрудник не работает!")
